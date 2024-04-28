b0363939aa81358014b10e36d9ade1c51d3b2cbe
isabelzulawski <151580629+isabelzulawski@users.noreply.github.com>
2024-04-28 12:41:33 -0400

    tiles.c

---
+++ 2048/tiles.c
@@ -0,0 +1,31 @@
+#include "game.h"
+
+//function to calculate max tile value. 
+int maxtile(game *c){
+    int max = c->tiles[0][0].value; 
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            int val = c->tiles[i][j].value;
+            if (val > max) { 
+                max = val; 
+            }
+
+        }
+    }
+    return max;
+}
+
+//function to generate random tile location and value.
+void randtile(game* c) {
+    int d = (rand() % 2) ? 2 : 4;
+    int i, j;
+    do {
+        i = rand() % SIZE; 
+        j = rand() % SIZE; 
+    } while (c->tiles[i][j].value != 0); 
+
+    //updates values of tile struct
+    c->tiles[i][j].value = d;
+    c->tiles[i][j].locationx = i;
+    c->tiles[i][j].locationy = j;
+}
